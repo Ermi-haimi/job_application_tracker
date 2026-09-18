a3bc78de87d185a4e21237f2a87bdf39bc73fbee
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-09-18 16:46:29 +0300

    styled application card

--- src/components/applicationCard/ApplicationCard.tsx
+++ src/components/applicationCard/ApplicationCard.tsx
@@ -1,4 +1,5 @@
 import type { JobApplication } from "../../types/application";
+import "./ApplicationCard.css";
 
 interface ApplicationCardProps {
   application: JobApplication;
@@ -9,20 +10,31 @@ export default function ApplicationCard({ application }: ApplicationCardProps) {
     <section className="app-card-wrapper">
       <h3>{application.position}</h3>
       <div className="company-wrapper">
-        <p>Company : </p>
+        <p>Company: </p>
         <p>{application.company}</p>
       </div>
       <div className="jobtype-salary-wrapper">
         <p>{application.jobType}</p>
-        <p>{application.salary ?? "Note Specified"}</p>
+        <div className="salary-wrapper">
+          <p>Salary</p> <p>{application.salary ?? "Note Specified"}</p>
+        </div>
       </div>
       <div className="note-wrapper">
         <p>{application.note ?? "No Note"}</p>
       </div>
+      <div className={`app-status ${application.status}`}>
+        {application.status}
+      </div>
       <div className="status-dates-wrapper">
-        <div className="app-status">{application.status}</div>
-        <div className="applied-date">{application.dateApplied ?? ""}</div>
-        <div className="deadline">{application.deadline ?? ""}</div>
+        <div className="applied-date">
+          <p>Applied Date</p>
+          <p>{application.dateApplied ?? ""}</p>
+        </div>
+
+        <div className="deadline">
+          <p>Deadline</p>
+          <p>{application.deadline ?? ""}</p>
+        </div>
       </div>
     </section>
   );
