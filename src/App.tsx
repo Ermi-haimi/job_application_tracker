c864dafd37f2efc7470d8f143d24751fe5215a24
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-09-20 23:38:23 +0300

    styled the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 
 export default function App() {
   return (
     <>
+      <Header />
       <Home />
     </>
   );
